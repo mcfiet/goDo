7004586303365a04e22ed87954f0dc950af143fa
Fiete Scheel <fietescheel@gmail.com>
2024-11-04 07:34:39 +0100

    Add authentification middleware to todo route

--- todo/routing/todo_router.go
+++ todo/routing/todo_router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/mcfiet/goDo/todo/controller"
+	"github.com/mcfiet/goDo/todo/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -11,11 +12,15 @@ import (
 func TodoRouter() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", controller.GetHello)
-	r.Route("/todos", func(r chi.Router) {
-		r.Get("/", controller.GetAllTodos)
-		r.Post("/", controller.CreateTodo)
-		r.Get("/{id}", controller.GetTodoById)
-		r.Put("/{id}", controller.UpdateTodoById)
+
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.UseIsAuthenticated)
+		r.Route("/todos", func(r chi.Router) {
+			r.Get("/", controller.GetAllTodos)
+			r.Post("/", controller.CreateTodo)
+			r.Get("/{id}", controller.GetTodoById)
+			r.Put("/{id}", controller.UpdateTodoById)
+		})
 	})
 
 	return r
