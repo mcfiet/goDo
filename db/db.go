031fffcd9c3a166af28b67b318268c6c172f0dbf
Fiete Scheel <fietescheel@gmail.com>
2024-11-12 07:36:37 +0100

    Remove panic

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"log"
+
 	"github.com/mcfiet/goDo/todo/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,7 +14,7 @@ func Init() {
 	dsn := "host=localhost user=postgres password=123456 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	db.AutoMigrate(&model.Todo{})
--- login/controller/login_controller.go
+++ login/controller/login_controller.go
@@ -1,16 +1,51 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/mcfiet/goDo/login/model"
 )
+
+var store = sessions.NewCookieStore([]byte("secret"))
 
 func GetLogin(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement
 	fmt.Println("Login GET")
+	session, err := store.Get(r, "session-name")
+	if err != nil {
+		http.Error(w, "Konnte Session nicht erstellen", http.StatusInternalServerError)
+	}
+
+	fmt.Println(session.Values)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement
 	fmt.Println("Login POST")
+	var user model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Println(user)
+
+	if user.Username == "fiete" && user.Password == "test" {
+		fmt.Println("Logged In")
+		session, err := store.Get(r, "session-name")
+		if err != nil {
+			http.Error(w, "Konnte Session nicht erstellen", http.StatusInternalServerError)
+		}
+
+		session.Values["user"] = user.Username
+		if err := session.Save(r, w); err != nil {
+			http.Error(w, "Konnte Session nicht speichern", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	}
+
 }
