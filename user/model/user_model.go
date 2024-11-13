7dcc7b29089ba08df2122647bb3f50c53a290c7a
Fiete Scheel <fietescheel@gmail.com>
2024-11-13 17:47:41 +0100

    Add Email to user #3

--- user/model/user_model.go
+++ user/model/user_model.go
@@ -4,6 +4,7 @@ import "github.com/mcfiet/goDo/utils"
 
 type User struct {
 	utils.Base
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" gorm:"unique,not null"`
+	Email    string `json:"email" gorm:"unique,not null"`
+	Password string `json:"password" gorm:"not null"`
 }
